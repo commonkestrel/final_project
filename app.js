7be40c0587943b54d5d9b34de85d23e1565761fa
Jibble330 <jettb6@gmail.com>
2022-05-31 22:52:11 -0700

    account system

--- app.js
+++ app.js
@@ -1,32 +1,61 @@
-const path = require('path')
-const bodyParser = require('body-parser')
-const express = require('express')
-const app = express()
-const port = 3000
+const path = require('path');
+const bodyParser = require('body-parser');
+const bcrypt = require("bcrypt")
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const users = [];
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/three', (req, res) => {
-    res.sendFile(path.join(__dirname, 'HTML', 'three.html'))
+app.use('/static', express.static(path.join(__dirname, 'static')));
+
+app.get("/", (req, res) => {
+    res.redirect("/login")
 })
 
-app.get('/school', (req, res) => {
-    console.log(req.method)
+app.post('/users/delete', (req, res) => {
+    const email = req.body.email;
+    users = users.filter((value, index, arr) => {
+        return value != users.find(user => user.email = req.body.email);
+    })
+})
+
+app.post('/users/create', async (req, res) => {
+    try {
+        if (users.find(user => user.email = req.body.email)) {
+            return res.send(false)
+        }
+        const hashedPassword = await bcrypt.hash(req.body.pass, 10)
+        const user = {"email": req.body.email, "password": hashedPassword}
+        users.push(user);
+        res.send(true)
+    } catch (err) {
+        console.log(err)
+        res.status(500).send()
+    }
+});
+
+app.post('/users/login', (req, res) => {
+    const user = users.find(user => user.email = req.body.email)
+    if (user == null) {
+        return res.status(400).send('User not found')
+    }
+    try{
+        if (bcrypt.compare(req.body.pass, user.password)) {
+            res.send(true)
+        } else {
+            res.send(false)
+        }
+    } catch {
+        res.status(500).send()
+    }
+})
+
+app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'HTML', 'school.html'))
-})
-
-app.use('/static', express.static(path.join(__dirname, 'static')))
-
-app.get('/bar', (req, res) => {
-    res.sendFile(path.join(__dirname, 'HTML', 'bar.html'))
-})
-
-app.post('/school', (req, res) => {
-    console.log(req.method)
-    console.log(req.body.email)
-    console.log(req.body.pass)
-    console.log(req.body.pass == req.body.confirm ? 'YES' : 'no')
 })
 
 app.listen(port, () => {
