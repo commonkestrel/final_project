8d370636a7c26df68f8b40e03b963020f38d9be6
Jibble330 <jettb6@gmail.com>
2022-06-01 16:12:15 +0000

    cookies

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const bcrypt = require("bcrypt")
 const express = require('express');
+const cookieParser = require("cookie-parser")
 const app = express();
 const port = 3000;
 
@@ -9,6 +10,7 @@ const users = [];
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cookieParser())
 
 app.use('/static', express.static(path.join(__dirname, 'static')));
 
@@ -21,6 +23,7 @@ app.post('/users/delete', (req, res) => {
     users = users.filter((value, index, arr) => {
         return value != users.find(user => user.email = req.body.email);
     })
+    res.clearCookie("Name").send()
 })
 
 app.post('/users/create', async (req, res) => {
@@ -31,7 +34,11 @@ app.post('/users/create', async (req, res) => {
         const hashedPassword = await bcrypt.hash(req.body.pass, 10)
         const user = {"email": req.body.email, "password": hashedPassword}
         users.push(user);
-        res.send(true)
+        res.cookie("Name", user.email, {
+                httpOnly: false,
+                maxAge: 1000 * 60 * 60 * 24 * 30 * 12
+            }).send(true)
+        console.log("Cookie Added")
     } catch (err) {
         console.log(err)
         res.status(500).send()
@@ -45,7 +52,10 @@ app.post('/users/login', (req, res) => {
     }
     try{
         if (bcrypt.compare(req.body.pass, user.password)) {
-            res.send(true)
+            res.cookie("Name", user.email, {
+                http: false,
+                maxAge: 1000 * 60 * 60 * 24 * 30 * 12
+            }).send(true)
         } else {
             res.send(false)
         }
